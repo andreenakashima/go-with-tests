ecfc13a548a9cc26752c003418c53f2c7aacf3c9
andreenakashima <andreenakashima@gmail.com>
2024-03-11 14:35:10 -0300

    add repeat count for Repeat function

--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -3,10 +3,12 @@ package iteration
 import "testing"
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
+	t.Run("Repeating a character 5 times", func(t *testing.T) {
+		repeated := Repeat("a", 5)
+		expected := "aaaaa"
 
-	if repeated != expected {
-		t.Errorf("expected %q but got %q", expected, repeated)
-	}
+		if repeated != expected {
+			t.Errorf("expected %q but got %q", expected, repeated)
+		}
+	})
 }
