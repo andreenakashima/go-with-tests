ce642367e8bec08e719433b1b87d2aa1a9354ebf
andreenakashima <andreenakashima@gmail.com>
2024-03-12 14:40:46 -0300

    refactor search function

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -3,15 +3,35 @@ package maps
 import "testing"
 
 func TestSearch(t *testing.T) {
-	dictionary := map[string]string{"test": "this is just a test"}
+	dictionary := Dictionary{"test": "this is just a test"}
 
-	got := Search(dictionary, "test")
-	want := "this is just a test"
+	t.Run("Known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
 
-	assertString(t, got, want)
+		assertStrings(t, got, want)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+		want := "could not find the word you were looking for"
+
+		if err == nil {
+			t.Fatal("expected to get a error")
+		}
+
+		assertStrings(t, err.Error(), want)
+	})
 }
 
-func assertString(t testing.TB, got, want string) {
+func asserError(t *testing.T, got, want error) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got error %q, want %q", got, want)
+	}
+}
+
+func assertStrings(t testing.TB, got, want string) {
 	t.Helper()
 
 	if got != want {
