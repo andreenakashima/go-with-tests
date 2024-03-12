2c6cbafa1865dc958c4074722eef266d3b7aa1cd
andreenakashima <andreenakashima@gmail.com>
2024-03-12 11:58:01 -0300

    add error message when insufficient founds occurs

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -1,6 +1,9 @@
 package pointers
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type Bitcoin int
 
@@ -12,17 +15,23 @@ type Wallet struct {
 	balance Bitcoin
 }
 
+var ErrInsufficientFunds = errors.New("cannot withdraw, isufficient funds")
+
 func (b Bitcoin) String() string {
 	return fmt.Sprintf("%d BTC", b)
 }
 
 func (w *Wallet) Deposit(amount Bitcoin) {
-	fmt.Printf("address in Deposit is %p \n", &w.balance)
 	w.balance += amount
 }
 
-func (w *Wallet) Withdraw(amount Bitcoin) {
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return ErrInsufficientFunds
+	}
+
 	w.balance -= amount
+	return nil
 }
 
 func (w *Wallet) Balance() Bitcoin {
