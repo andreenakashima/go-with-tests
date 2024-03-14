e7cbcd54ce76db2b7aea682101d338b6a40a5b92
andreenakashima <andreenakashima@gmail.com>
2024-03-14 09:42:09 -0300

    add a error if a server dont responds in specified time

--- select/racer_test.go
+++ select/racer_test.go
@@ -8,22 +8,34 @@ import (
 )
 
 func TestRacer(t *testing.T) {
-	slowServer := makeDelayServer(20 * time.Millisecond)
+	t.Run("compare speed of servers, return url of the fastest one", func(t *testing.T) {
+		slowServer := makeDelayServer(20 * time.Millisecond)
+		fastServer := makeDelayServer(0 * time.Millisecond)
 
-	fastServer := makeDelayServer(0 * time.Millisecond)
+		defer slowServer.Close()
+		defer fastServer.Close()
 
-	slowURL := slowServer.URL
-	fastURL := fastServer.URL
+		slowURL := slowServer.URL
+		fastURL := fastServer.URL
 
-	want := fastURL
-	got := Racer(slowURL, fastURL)
+		want := fastURL
+		got, _ := Racer(slowURL, fastURL)
 
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
 
-	slowServer.Close()
-	fastServer.Close()
+	t.Run("returns a error if a server doesn't respond within specified time", func(t *testing.T) {
+		server := makeDelayServer(25 * time.Millisecond)
+
+		defer server.Close()
+
+		_, err := ConfigurableRacer(server.URL, server.URL, 20*time.Millisecond)
+		if err == nil {
+			t.Error("expected error but didn't get one")
+		}
+	})
 }
 
 func makeDelayServer(delay time.Duration) *httptest.Server {
