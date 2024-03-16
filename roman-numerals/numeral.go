9af91eca6dbbc4e22aaa093d5f63acb7b2876b75
andreenakashima <andreenakashima@gmail.com>
2024-03-16 15:02:44 -0300

    add behavior with 4 numeral

--- roman-numerals/numeral.go
+++ roman-numerals/numeral.go
@@ -6,6 +6,10 @@ func ConvertToRoman(arabic int) string {
 	var result strings.Builder
 
 	for i := 0; i < arabic; i++ {
+		if arabic == 4 {
+			result.WriteString("IV")
+			break
+		}
 		result.WriteString("I")
 	}
 
