adba66ea94b255f27b9c68ca5f7604b4c330e61c
andreenakashima <andreenakashima@gmail.com>
2024-03-19 13:48:43 -0300

    add hour in radians function

--- math/clockface_test.go
+++ math/clockface_test.go
@@ -84,6 +84,26 @@ func TestMinuteHandPoint(t *testing.T) {
 	}
 }
 
+func TestHoursInRadians(t *testing.T) {
+	cases := []struct {
+		time  time.Time
+		angle float64
+	}{
+		{simpleTime(6, 0, 0), math.Pi},
+		{simpleTime(0, 0, 0), 0},
+		{simpleTime(21, 0, 0), math.Pi * 1.5},
+	}
+
+	for _, c := range cases {
+		t.Run(testName(c.time), func(t *testing.T) {
+			got := hoursInRadians(c.time)
+			if got != c.angle {
+				t.Fatalf("wanted %v radians, but got %v", c.angle, got)
+			}
+		})
+	}
+}
+
 func simpleTime(hours, minutes, seconds int) time.Time {
 	return time.Date(312, time.October, 28, hours, minutes, seconds, 0, time.UTC)
 }
