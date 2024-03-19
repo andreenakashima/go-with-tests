0dec0fd69aa0ccd835b14d0155b99781a664a6e2
andreenakashima <andreenakashima@gmail.com>
2024-03-19 10:15:39 -0300

    add drawing minute hand

--- math/clockface_acceptance_test.go
+++ math/clockface_acceptance_test.go
@@ -71,10 +71,10 @@ func TestSVGWriterMinuteHand(t *testing.T) {
 		time time.Time
 		line Line
 	}{
-		// {
-		// 	simpleTime(0, 0, 0),
-		// 	Line{150, 150, 150, 70},
-		// },
+		{
+			simpleTime(0, 0, 0),
+			Line{150, 150, 150, 70},
+		},
 	}
 
 	for _, c := range cases {
